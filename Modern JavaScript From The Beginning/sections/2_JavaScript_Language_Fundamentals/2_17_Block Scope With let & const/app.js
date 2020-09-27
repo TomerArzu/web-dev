f67770eb0a664a9374adebe5762345a1f2e2b761
Tomer Arzuan_Personal <Tomerarzu@gmail.com>
2020-09-27 15:33:08 +0300

    var, let, const varibles

--- Modern JavaScript From The Beginning/sections/2_JavaScript_Language_Fundamentals/2_17_Block Scope With let & const/app.js
+++ Modern JavaScript From The Beginning/sections/2_JavaScript_Language_Fundamentals/2_17_Block Scope With let & const/app.js
@@ -1,8 +1,9 @@
-// Global Scope
+//? Global Scope
 var a = 1;
 let b = 2;
 const c = 3;
 
+//? function scope
 // function test() {
 //   var a = 4;
 //   let b = 5;
@@ -12,16 +13,22 @@ const c = 3;
 
 // test();
 
+//?  Block Scope
 // if(true) {
-//   // Block Scope
+//*  the 'var' variable is, initially, declare as global
+//*  when we change it in the block level scope it will change outside ether assignment
 //   var a = 4;
+//* let isn't changing in outside of the block level scope
 //   let b = 5;
 //   const c = 6;
 //   console.log('If Scope: ', a, b, c);
 // }
 
+// * in the end of the loop, a will be equal to 10 in the outside of the scope
 // for(var a = 0; a < 10; a++) {
 //   console.log(`Loop: ${a}`);
 // }
 
-console.log('Global Scope: ', a, b, c);
+// ! var changes the value of variable in the global scope from the block scope (but not from function scope)
+
+console.log("Global Scope: ", a, b, c);
