af67577fd59bc1f15242435deed72cb0402b1bc5
Tomer Arzuan_Personal <Tomerarzu@gmail.com>
2020-10-03 20:04:51 +0300

    document interface and single element selector

--- Modern JavaScript From The Beginning/sections/3_DOM_Manipulation&Events/3_2_Examining_The_Document_Object/app.js
+++ Modern JavaScript From The Beginning/sections/3_DOM_Manipulation&Events/3_2_Examining_The_Document_Object/app.js
@@ -1,17 +1,34 @@
 let val;
 
+// * the document is an interface for the HTML
+// * the document defines HTML element as object, HTML elements properties,methods, event
+// * The HTML DOM is a standard for how to get, change, add, or delete HTML elements
 val = document;
+console.log("document", val);
+
+// ? following are some of the document interface properties:
+// * all return us the all elements in the HTML as a collection
+// ! the all is Deprecated we use getElementById() instead
 val = document.all;
+console.log("document.all", val);
+// * we can select or access element in the HTML with all and index as follow:
 val = document.all[2];
 val = document.all.length;
+// * we can access to any parts of the document by its properties
+// * if we will access the head it will return us the HTML code itself (also for body as so...)
 val = document.head;
+console.log("document.head", document.head);
 val = document.body;
 val = document.doctype;
+console.log("document.doctype", document.doctype);
 val = document.domain;
 val = document.URL;
 val = document.characterSet;
+console.log("document.characterSet", document.characterSet);
 val = document.contentType;
+console.log("document.contentType", document.contentType);
 
+// * the following is demonstrate us that we can selects elements without using selectors (but is not recommended)
 val = document.forms;
 val = document.forms[0];
 val = document.forms[0].id;
@@ -27,14 +44,14 @@ val = document.links[0].classList[0];
 val = document.images;
 
 val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// * we can access the attributes of some elements by access into the element itself
+// * and we also need to describe the element name.
+val = document.scripts[2].getAttribute("src");
 
+// * we can iterate over HTML elements collection
+// * by selecting the elements and turn them into array (because its not arrays by default)
 let scripts = document.scripts;
-
 let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
+scriptsArr.forEach(function (script) {
+	console.log("script", script.getAttribute("src"));
 });
-
-console.log(val);
--- Modern JavaScript From The Beginning/sections/3_DOM_Manipulation&Events/3_3_DOM_Selectors_For_Single_Elements/app.js
+++ Modern JavaScript From The Beginning/sections/3_DOM_Manipulation&Events/3_3_DOM_Selectors_For_Single_Elements/app.js
@@ -1,39 +1,78 @@
-// document.getElementById()
+// ? DOM SELECTORS - querySelector
+// * document object method that allows us to pull elements from the DOM
+// * and manipulate them
+// * jQuery is used to do DOM manipulation and selection but vanilla JS (the updated one)
+// * is got much easier to work with so we don't have to use jQuery library (more efficient)
+// TODO: document.getElementById()
+// ! jQuery is useful for quick simple plugin or scripts that we need to use for specific action,
+// ! but not for DOM manipulation.
 
+// * we can categorize the selector into to main types
+// *    1. single element selector (Two types we will look right now) - one element by id/class and its variable stored one HTML element
+// *    2. multiple element selector - will select all of the elements in that class and return HTMLCollection/NodeList
 // console.log(document.getElementById('task-title'));
 
-// // Get things from the element
-// console.log(document.getElementById('task-title').id);
-// console.log(document.getElementById('task-title').className);
+// ? Get things from the element
+console.log(
+	"getElementById('task-title').id:",
+	document.getElementById("task-title").id
+);
+console.log(
+	"getElementById('task-title').className:",
+	document.getElementById("task-title").className
+);
 
-// const taskTitle = document.getElementById('task-title');
+const taskTitle = document.getElementById("task-title");
 
-// // Change styling
+// ? Change styling
 // taskTitle.style.background = '#333';
 // taskTitle.style.color = '#fff';
 // taskTitle.style.padding = '5px';
 // // taskTitle.style.display = 'none';
 
-// // Change content
+// ?  Change content
+// * we have 3 different ways to change HTML elements content
+// * we will apply this properties after selecting the changeable element
+// * textContent
 // taskTitle.textContent = 'Task List';
+// * innerText
 // taskTitle.innerText = 'My Tasks';
-// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+// * innerHTML - we use this to "push" into the element more HTML element
+// * it apply we string between the open and closer HTML tag.
+// * here, for example, we selected the h5 element so after rendering we can inspect the HTML page
+// * and we can see that we have <h5 id="task-title" (..attr..)><span (..attr..)></span></h5>
+// * it is usually used with the use of fetch or AJAX
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+// TODO: NOTICE ! the innerHTML replace the innerHTML that already inside the tag
 
-// document.querySelector()
+// ? querySelector
+// * a newer and much more powerful selector.
+// * with this selector we don't need to select element by id only
+// * though, we can select by id either by class.
+// * like CSS Selectors:
+// *    use: #[id] to select by id
+// *    use: .[class] to select by class
+// *    or just use: [HTML-ELEMENT] to select by class
+// TODO: document.querySelector()
+// ! Any of CSS Selectors is working here also
 
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+// * we can see that the selector return us all of the HTML line
+console.log(document.querySelector("#task-title"));
+console.log(
+	"document.querySelector('#task-title')",
+	document.querySelector("#task-title")
+);
+console.log(document.querySelector(".card-title"));
+console.log(document.querySelector("h5"));
 
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
+document.querySelector("li").style.color = "red";
+document.querySelector("ul li").style.color = "blue";
 
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(3)').style.color = 'yellow';
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
-
-
-
-
+document.querySelector("li:last-child").style.color = "red";
+document.querySelector("li:nth-child(3)").style.color = "yellow";
+document.querySelector("li:nth-child(4)").textContent = "Hello World";
+// * the following line will change only the first line because:
+// * li:nth-child(odd) is usually return more then one item (id n > 2)
+// * But, querySelector is a single element selector so it will select the first odd in the list
+document.querySelector("li:nth-child(odd)").style.background = "#ccc";
+document.querySelector("li:nth-child(even)").style.background = "#f4f4f4";
