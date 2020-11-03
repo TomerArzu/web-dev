451dd2aa7a686b973bbe02d313d1e48bfae8a4e9
Tomer Arzuan <Tomer@korentec.co.il>
2020-11-03 18:04:16 +0200

    fied small issue in the NumberGuesser game

--- Modern JavaScript From The Beginning/sections/4_DOM_Projects/Number_Guesser/Number Guesser [Part 4] - Play Again/app.js
+++ Modern JavaScript From The Beginning/sections/4_DOM_Projects/Number_Guesser/Number Guesser [Part 4] - Play Again/app.js
@@ -8,16 +8,16 @@ GAME FUNCTION:
 */
 
 let min = 1,
-	max = 10,
-	winningNum = getRandomNum(min, max),
-	guessesLeft = 3;
+  max = 10,
+  winningNum = getRandomNum(min, max),
+  guessesLeft = 3;
 
 const GameEL = document.querySelector("#game"),
-	minNumEL = document.querySelector(".min-num"),
-	maxNumEL = document.querySelector(".max-num"),
-	guessBtn = document.querySelector("#guess-btn"),
-	guessInput = document.querySelector("#guess-input"),
-	messageEL = document.querySelector(".message");
+  minNumEL = document.querySelector(".min-num"),
+  maxNumEL = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  messageEL = document.querySelector(".message");
 
 // ! Add event delegation !
 // * since the class "play-again" of the button "guessBtn" added after page load,
@@ -26,63 +26,64 @@ const GameEL = document.querySelector("#game"),
 // TODO: when we apply on the following event listener the 'click' event, of the change to "play-again" we catch the "mouseup" event and it fires the event immediately,
 // TODO: so we want to apply it only when mouse down
 game.addEventListener("mousedown", function (e) {
-	if (e.target.className === "play-again") {
-		window.location.reload();
-	}
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
 });
 
 minNumEL.textContent = min;
 maxNumEL.textContent = max;
 
 guessBtn.addEventListener("click", function (e) {
-	console.log("guessBtn clicked... \nValue:", guessInput.value);
-	let guess = parseInt(guessInput.value);
+  console.log("guessBtn clicked... \nValue:", guessInput.value);
+  let guess = parseInt(guessInput.value);
 
-	if (isNaN(guess) || guess < min || guess > max) {
-		setMessage(`Please Enter Number between ${min} and ${max}`, "red");
-	} else {
-		if (guess === winningNum) {
-			gameOver(true, `${winningNum} is correct , YOU WIN !`, "green");
-		} else {
-			guessesLeft--;
-			if (guessesLeft === 0) {
-				gameOver(
-					false,
-					`Game over - you lost ! the correct number was ${winningNum}`
-				);
-			} else {
-				setMessage(
-					`${guess} is not correct - ${guessesLeft} guesses left`,
-					"red"
-				);
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please Enter Number between ${min} and ${max}`, "red");
+  } else {
+    if (guess === winningNum) {
+      gameOver(true, `${winningNum} is correct , YOU WIN !`, "green");
+    } else {
+      guessesLeft--;
+      if (guessesLeft === 0) {
+        gameOver(
+          false,
+          `Game over - you lost ! the correct number was ${winningNum}`
+        );
+      } else {
+        setMessage(
+          `${guess} is not correct - ${guessesLeft} guesses left`,
+          "red"
+        );
 
-				guessInput.style.borderColor = "red";
-				guessInput.value = "";
-			}
-		}
-	}
+        guessInput.style.borderColor = "red";
+        guessInput.value = "";
+      }
+    }
+  }
 });
 
 function gameOver(won, msg) {
-	let color;
-	color = won === true ? "green" : "red";
-	guessInput.disabled = true;
-	guessInput.style.borderColor = color;
-	setMessage(msg, color);
+  let color;
+  color = won === true ? "green" : "red";
+  guessInput.textContent = " ";
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  setMessage(msg, color);
 
-	// * play again ?
-	guessBtn.value = "Play Again";
-	// * We append the class to the button "play-again"
-	guessBtn.className += "play-again";
+  // * play again ?
+  guessBtn.value = "Play Again";
+  // * We append the class to the button "play-again"
+  guessBtn.className += "play-again";
 }
 
 function getRandomNum(min, max) {
-	num = Math.floor(Math.random() * (max - min + 1) + min);
-	console.log(num);
-	return num;
+  num = Math.floor(Math.random() * (max - min + 1) + min);
+  console.log(num);
+  return num;
 }
 
 function setMessage(msg, color) {
-	messageEL.style.color = color;
-	messageEL.textContent = msg;
+  messageEL.style.color = color;
+  messageEL.textContent = msg;
 }
