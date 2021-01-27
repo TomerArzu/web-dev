e2de289e0d5d0f454e371105468402cc637552ba
Tomer Arzuan <Tomer@korentec.co.il>
2021-01-27 16:11:29 +0200

    added some comments after recap

--- Modern JavaScript From The Beginning/sections/4_DOM_Projects/Task_list/Task List [Part 1] - UI & Add Task/app.js
+++ Modern JavaScript From The Beginning/sections/4_DOM_Projects/Task_list/Task List [Part 1] - UI & Add Task/app.js
@@ -2,34 +2,34 @@
 const form = document.querySelector("#task-form");
 const collection = document.querySelector(".collection");
 const clearBtn = document.querySelector(".clear-tasks");
-const filter = document.querySelector("#filter");
+const filterInput = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
 // * load all event listeners
 attachEventListener();
 function attachEventListener() {
-	form.addEventListener("submit", addTask);
+    form.addEventListener("submit", addTask);
 }
 
 function addTask(e) {
-	if (taskInput.value === "") {
-		alert("Add a task");
-	} else {
-		// * create the task's UI (li)
-		const li = document.createElement("li");
-		li.className = "collection-item";
-		li.appendChild(document.createTextNode(taskInput.value));
-		const link = document.createElement("a");
-		link.href = "#";
-		link.className = "delete-item secondary-content";
-		link.innerHTML = "<i class='fa fa-remove'></i>";
-		li.appendChild(link);
+    if (taskInput.value === "") {
+        alert("Add a task");
+    } else {
+        // * create the task's UI (li)
+        const li = document.createElement("li");
+        li.className = "collection-item";
+        li.appendChild(document.createTextNode(taskInput.value));
+        const link = document.createElement("a");
+        link.href = "#";
+        link.className = "delete-item secondary-content";
+        link.innerHTML = "<i class='fa fa-remove'></i>";
+        li.appendChild(link);
 
-		// * append the li to ul
-		collection.appendChild(li);
+        // * append the li to ul
+        collection.appendChild(li);
 
-		// * clear the input
-		taskInput.value = "";
-	}
-	e.preventDefault();
+        // * clear the input
+        taskInput.value = "";
+    }
+    e.preventDefault();
 }
